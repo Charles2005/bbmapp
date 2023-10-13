6d3a20e43475eb72009eacb288f090a0a16c81aa
Charles Abe <abecharles5@gmail.com>
2023-10-13 10:28:14 +0800

    default display 30 day interval

--- php/allMarket.php
+++ php/allMarket.php
@@ -3,7 +3,7 @@ include("../config/config.php");
 
 
 $query = "SELECT product_qty, product_price, region, shop_name, date_created FROM bbm_data 
-WHERE product_name = 'Bitter gourd / Ampalaya' AND shop_name = 'Pasay City Wet Market' and date_created between '2023-09-01' and '2023-09-30'";
+WHERE product_name = 'Bitter gourd / Ampalaya' AND shop_name = 'Pasay City Wet Market' and DATE_SUB(CURDATE(), INTERVAL 30 DAY) <= date_created";
 
 
 $result = mysqli_query($conn, $query); 
